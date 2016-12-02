8c791da83ee0c8cc2a404b91e786b4b8ae1737f6
Carlos Pereira <carlinhop@gmail.com>
2016-12-02 11:21:10 +0000

    Basic Store object

--- specs/Store_spec.js
+++ specs/Store_spec.js
@@ -1,4 +1,31 @@
 var assert = require("assert");
 var Record = require("../Record");
-var Record = require("../Store");
+var Store = require("../Store");
 
+describe("Store", function(){
+  var store1;
+
+  before(function(){
+    store1 = new Store("Gozadera Records", "Caracas");
+  });
+
+  it("should exist", function(){
+    assert(store1);
+  });
+
+  it("should have a name", function(){
+    assert.equal("Gozadera Records", store1.name);
+  });
+
+  it("should have a city", function(){
+    assert.equal("Caracas", store1.city);
+  });
+
+  it("should have an inventory", function(){
+    assert.deepEqual([],store1.inventory);
+  });
+
+  
+
+
+});
